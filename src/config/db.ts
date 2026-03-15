769e3c37b25fea410da2d44ed711672993491666
jaber <jaberhc2002@gmail.com>
2026-03-15 19:45:21 +0600

    add : ph source code

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,49 +1,39 @@
 import { Pool } from "pg";
-import config from "./index.ts";
+import config from ".";
 
-if (!process.env.CONNECTION_STRING) {
-  throw new Error("CONNECTION_STRING missing in .env");
-}
-
-const pool = new Pool({
-  connectionString: config.connection_string,
-  ssl: { rejectUnauthorized: false },
-  connectionTimeoutMillis: 10000,
+//DB
+export const pool = new Pool({
+  connectionString: `${config.connection_str}`,
 });
 
 const initDB = async () => {
-  try {
-    await pool.query(`
-      CREATE TABLE IF NOT EXISTS users(
+  await pool.query(`
+        CREATE TABLE IF NOT EXISTS users(
         id SERIAL PRIMARY KEY,
         name VARCHAR(100) NOT NULL,
+        role VARCHAR(50) NOT NULL,
         email VARCHAR(150) UNIQUE NOT NULL,
+        password TEXT NOT NULL,
         age INT,
         phone VARCHAR(15),
         address TEXT,
         created_at TIMESTAMP DEFAULT NOW(),
         updated_at TIMESTAMP DEFAULT NOW()
-      )
-    `);
+        )
+        `);
 
-    await pool.query(`
-      CREATE TABLE IF NOT EXISTS todos(
-        id SERIAL PRIMARY KEY,
-        user_id INT REFERENCES users(id) ON DELETE CASCADE,
-        title VARCHAR(200) NOT NULL,
-        description VARCHAR(400),
-        completed BOOLEAN DEFAULT false,
-        due_date DATE,
-        created_at TIMESTAMP DEFAULT NOW(),
-        updated_at TIMESTAMP DEFAULT NOW()
-      )
-    `);
-
-    console.log("Tables created or already exist");
-  } catch (err) {
-    console.error("Error creating tables:", err);
-    throw err;
-  }
+  await pool.query(`
+            CREATE TABLE IF NOT EXISTS todos(
+            id SERIAL PRIMARY KEY,
+            user_id INT REFERENCES users(id) ON DELETE CASCADE,
+            title VARCHAR(200) NOT NULL,
+            description TEXT,
+            completed BOOLEAN DEFAULT false,
+            due_date DATE,
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
+            )
+            `);
 };
 
-export { pool, initDB };
+export default initDB;
--- src/server.ts
+++ src/server.ts
@@ -1,39 +1,8 @@
-import userRoute from "./routes/users.ts";
-import { initDB, pool } from "./config/db.ts";
-import { homeRoute } from "./routes/home.ts";
-import app from "./app.ts";
-import todosRoute from "./routes/todos.ts";
-import logger from "./middleware/logger.ts";
-import config from "./config/index.ts";
+import app from "./app";
+import config from "./config";
 
-const port = process.env.PORT || 5000;
+const port = config.port;
 
-app.use(logger);
-app.use("/", homeRoute);
-app.use("/api", userRoute);
-app.use("/api", todosRoute);
-app.use((req, res) => {
-  res.status(404).json({
-    success: false,
-    message: "route dont found",
-    path: req.path,
-  });
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
-
-const startServer = async () => {
-  try {
-    await pool.connect();
-    console.log("Database connected");
-
-    await initDB();
-
-    app.listen(port, () => {
-      console.log(`Server running on port ${config.port}`);
-    });
-  } catch (error) {
-    console.error("Failed to start server:", error);
-    process.exit(1);
-  }
-};
-
-startServer();
